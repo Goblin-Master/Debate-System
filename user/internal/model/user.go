e1336f41808d9451e39eb495471ce02b246e95fa
Goblin-master <2896151192@qq.com>
2025-08-30 16:16:10 +0800

    fix: 修改初始化mysql的位置

--- user/internal/model/user.go
+++ user/internal/model/user.go
@@ -1,12 +1,5 @@
 package model
 
-import (
-	"Debate-System/pkg/gormx"
-	"fmt"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-)
-
 type User struct {
 	ID       int64  `gorm:"column:id;primary_key;AUTO_INCREMENT"`
 	UserID   int64  `gorm:"column:user_id;unique;type:bigint;not null"`
@@ -20,23 +13,4 @@ type User struct {
 
 func (User) TableName() string {
 	return "user"
-}
-func autoMigrate(db *gorm.DB) error {
-	err := db.AutoMigrate(
-		User{},
-	)
-	fmt.Println("数据库迁移成功")
-	return err
-}
-func InitDB(cfg gormx.Config, logger logger.Interface) *gorm.DB {
-	db, err := gormx.Open(cfg, logger)
-	if err != nil {
-		panic(err)
-	}
-	err = autoMigrate(db)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("数据库初始化成功")
-	return db
 }
